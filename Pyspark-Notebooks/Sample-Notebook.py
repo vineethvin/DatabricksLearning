ed352fae2488d7ba32fec96f3ade2f1449807c6a
vineeth <vineethvin2004@gmail.com>
2023-11-08 11:22:02 +0530

    Update Sample-Notebook.py
    Editing via the github directly

--- Pyspark-Notebooks/Sample-Notebook.py
+++ Pyspark-Notebooks/Sample-Notebook.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 # MAGIC %python
 # MAGIC print("note-book to test the integration with git")
+# MAGIC print("Now editing the notebook in github")
 
 # COMMAND ----------
 
