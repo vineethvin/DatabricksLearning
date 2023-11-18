0f4e23b413d3506a098e9a06733cb9394ed310a8
Vineethvin2004@gmail.com <Vineethvin2004@gmail.com>
2023-11-18 06:13:24 +0000

    add to check the commit from databricks

--- Pyspark-Notebooks/Sample-Notebook.py
+++ Pyspark-Notebooks/Sample-Notebook.py
@@ -3,6 +3,7 @@
 # MAGIC print("note-book to test the integration with git")
 # MAGIC print("added a line in devlopment")
 # MAGIC print("Now editing the notebook in github")
+# MAGIC print("latest line in databricks 11/18/2023")
 
 # COMMAND ----------
 
