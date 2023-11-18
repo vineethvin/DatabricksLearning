a2e777481f1007fb781ffa5d5ae174c39a28d730
Vineethvin2004@gmail.com <Vineethvin2004@gmail.com>
2023-11-18 06:18:43 +0000

    commit changes from dev

--- Pyspark-Notebooks/Sample-Notebook.py
+++ Pyspark-Notebooks/Sample-Notebook.py
@@ -3,6 +3,7 @@
 # MAGIC print("note-book to test the integration with git")
 # MAGIC print("added a line in devlopment")
 # MAGIC print("Now editing the notebook in github")
+# MAGIC print("aaded changes from development branch witout refreshing from main")
 
 # COMMAND ----------
 
